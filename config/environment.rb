c29490ef0b122ffab9141e4b2168def9d5825073
Duncan <duncanmorrison2001@yahoo.com>
2019-06-21 12:01:09 +0100

    Funcionality complete

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+
+# flickr = FlickRaw::Flickr.new "cd2975fbac81877754327544811e6bb5", "ff69eabe038b79e9"
